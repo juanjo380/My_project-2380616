3c0990e532435ab7f05f00d17f6e3c86369ee7f0
juanjo380 <juan.bolanos.serna@correounivalle.edu.co>
2023-09-28 08:56:45 -0500

    first commit

---
+++ My project.py
@@ -0,0 +1,24 @@
+#My project CRUD ->
+Clients = 'Luis,Kevin,'
+
+def create_client():
+    pass
+def read_client():
+    pass
+def update_client():
+    pass
+def delete_client():
+    pass
+
+
+def _print_welcome():
+    print("WELCOME TO UNIVERSIDAD DEL VALLE - TULUA")
+    print('*' * 100)
+    print("What would you like to do today:")
+    print("[C]reate client or user")
+    print("[R]ead client or user")
+    print("[U]pdate client or user")
+    print("[D]elete client or user")
+
+if __name__ == '__main__':
+    _print_welcome()
